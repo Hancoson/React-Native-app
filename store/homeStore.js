35e2997cb80617aba336e9375899bdce5be0da05
Hancoson <hancoson@163.com>
2017-11-23 18:18:19 +0800

    add mobx

---
+++ store/homeStore.js
@@ -0,0 +1,14 @@
+import default from "../components/item/index";
+
+/**
+ * @Author: Guoxing.han 
+ * @Date: 2017-11-23 16:11:44 
+ * @version 0.0.1 
+ * 首页store
+  */
+class HomeStore {
+
+}
+const homeStore = new HomeStore()
+
+export default homeStore
---
+++ store/index.js
@@ -0,0 +1,8 @@
+/**
+ * @Author: Guoxing.han 
+ * @Date: 2017-11-23 15:52:42 
+ * @version 0.0.1 
+  */
+import {homeStore} from './homeStore'
+
+export {homeStore}
