8f311a6b08878671f3d691c785bb13d28327d628
Hancoson <hancoson@163.com>
2017-11-26 13:00:43 +0800

    update commit

--- store/index.js
+++ store/index.js
@@ -2,7 +2,7 @@
  * @Author: Guoxing.han 
  * @Date: 2017-11-23 15:52:42 
  * @version 0.0.1 
-  */
-import {homeStore} from './homeStore'
+ */
+import { homeStore } from './homeStore'
 
-export {homeStore}
+export { homeStore }
